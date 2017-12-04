feef4ef4f101879a602a0d942d64c46d1caeaf64
Brian de Buiteach <debuiteb@tcd.ie>
2017-12-04 13:51:53 +0000

    main updated

--- Twitter_Gender_Class/main.py
+++ Twitter_Gender_Class/main.py
@@ -1,7 +1,19 @@
 #Main should just call the other files
-import support_vector_machine.py as svm
-import cluster.py as cluster
+import support_vector_machine as svm
+import cluster as cluster
 import preprocessing as preprocessing
+import pandas
+
+filepath = "" #path to csv file
+data_frame = preprocessing.readFile() # pandas dataframe
+
+data_frame = preprocessing.clean(data_frame)
+
+a = svm.do()
+b = cluster.do()
+
+print("A: " , a)
+print("B: " , b)
 
 print('starting...')
 
