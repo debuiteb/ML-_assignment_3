d888ff6920a12ef118eba45e869312ad5190d160
Gordon Hind <hindg@tcd.ie>
2017-12-04 13:58:36 +0000

    added readin to preprocessing

--- Twitter_Gender_Class/main.py
+++ Twitter_Gender_Class/main.py
@@ -1,1 +1,5 @@
 #Main should just call the other files
+import preprocessing
+import pandas
+
+data = preprocessing.readFile("C:\\Users\\gordo\\Desktop\\ML\\ML-_assignment_3\\Twitter_Gender_Class\\gender-classifier-DFE-791531.csv")
--- Twitter_Gender_Class/preprocessing.py
+++ Twitter_Gender_Class/preprocessing.py
@@ -1,1 +1,15 @@
 #Clean init dataset add text features remove unused
+import pandas
+import os
+
+
+def readFile(filepath):
+    column_names = ["_unit_id","_golden", "_unit_state","_trusted_judgements","_last_judgement_at","gender","gender:confidence",
+                    "profile_yn","profile_yn:confidence","created","description","fav_number","gender_gold","link_color","name",
+                    "profile_yn_gold","profileimage","retweet_count","sidebar_color","text","tweet_coord","tweet_count",
+                    "tweet_created","tweet_id","tweet_location","user_timezone"]
+    dataframe = pandas.read_csv(filepath,encoding="ISO-8859-1",
+                                sep=',', header=0, names=column_names,
+                                usecols=[5,9,10,11,13,18,19,21,22])
+    print(dataframe)
+    return dataframe
