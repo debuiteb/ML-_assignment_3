6357b13aaf1db156c05e58c867693889994aa2ee
Gordon Hind <hindg@tcd.ie>
2017-12-09 14:40:24 +0000

    check commit

--- Twitter_Gender_Class/preprocessing.py
+++ Twitter_Gender_Class/preprocessing.py
@@ -1,5 +1,7 @@
 #Clean init dataset add text features remove unused
-import pandas
+import pandas as pd
+import nltk
+from nltk import word_tokenize
 import os
 
 
@@ -8,12 +10,24 @@ def readFile(filepath):
                     "profile_yn","profile_yn:confidence","created","description","fav_number","gender_gold","link_color","name",
                     "profile_yn_gold","profileimage","retweet_count","sidebar_color","text","tweet_coord","tweet_count",
                     "tweet_created","tweet_id","tweet_location","user_timezone"]
-    dataframe = pandas.read_csv(filepath,encoding="ISO-8859-1",
+    dataframe = pd.read_csv(filepath,encoding="ISO-8859-1",
                                 sep=',', header=0, names=column_names,
                                 usecols=[5,9,10,11,13,18,19,21,22])
     print(dataframe)
     return dataframe
 
-def clean(filepath):
-    dataframe = "data"
+def clean(dataframe):
+    descriptions = dataframe.description
+    tweets = dataframe.text
+    tokenise(descriptions)
     return dataframe
+
+def tokenise(column):
+    for row in column:
+        print(row)
+        if not(pd.isnull(row)):
+            tokens = word_tokenize(row)
+            print(tokens)
+
+
+
