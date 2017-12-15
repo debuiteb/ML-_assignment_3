0e0b9d0e267af82e961cec2e1f9cd0c2a1517f36
Gordon Hind <hindg@tcd.ie>
2017-12-15 09:48:30 +0000

    knn init

--- Twitter_Gender_Class/knn.py
+++ Twitter_Gender_Class/knn.py
@@ -14,7 +14,11 @@ def knn_run(data_frame, k):
          'link_in_tweet', 'account_age', 'r_sidebar_colour', 'g_sidebar_colour', 'b_sidebar_colour', 'r_link_colour',
          'g_link_colour', 'b_link_colour']]
     X = preprocessing.feature_select_custom(X, Y, k)
-    clf = neighbors.KNeighborsClassifier(n_neighbors=3)
+    [n,m] = X.shape
+    #TOTRY Neighbours should be n^0.5, where n is the number of samples
+    #for i in range(2, 15):
+    # print("Neighbour number %i"%(i))
+    clf = neighbors.KNeighborsClassifier(n_neighbors=int(n**0.5))
     Y_fold = Y.head(int(set_size))
     kf2 = KFold(n_splits=10)
     kf2.get_n_splits(Y_fold)
